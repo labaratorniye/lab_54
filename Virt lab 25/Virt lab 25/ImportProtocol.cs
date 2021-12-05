9a01f0af95b344a48c65202dd9be7d8f4f230d9b
tuffdeblain <86976268+tuffdeblain@users.noreply.github.com>
2021-12-05 22:28:37 +0300

    protocol import form added

---
+++ Virt lab 25/Virt lab 25/ImportProtocol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Virt_lab_25
+{
+    public partial class ImportProtocol : Form
+    {
+        public ImportProtocol()
+        {
+            InitializeComponent();
+        }
+    }
+}
